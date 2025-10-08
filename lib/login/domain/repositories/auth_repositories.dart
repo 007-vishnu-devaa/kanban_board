7276f54ae0dfc9d02c967e54fb50406ab289eea3
Vishnu dev Pandey <vish.dev.pandey123@gmail.com>
2025-10-08 15:17:41 +0530

    Sign Up with Firebase done and Login page UI enhancement

--- lib/login/domain/repositories/auth_repositories.dart
+++ lib/login/domain/repositories/auth_repositories.dart
@@ -3,4 +3,5 @@ import '../model/user_entity.dart';
 
 abstract class AuthRepository {
   Future<UserEntity?> login(String email, String password);
+  Future<UserEntity?> signUp(String email, String password);
 }
