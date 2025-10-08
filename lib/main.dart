485f385c53016e1e2087baae5c0a0fb298d91fa7
Vishnu dev Pandey <vish.dev.pandey123@gmail.com>
2025-10-08 13:00:17 +0530

    Sign in functionality implemented with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kanbanboard/firebase_options.dart';
 import 'login/presentation/login_page.dart';
 
 
 Future<void> main() async {
-    // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //    options: DefaultFirebaseOptions.currentPlatform,
-  // );
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
